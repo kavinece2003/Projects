80359db3f78664c31357be55d80deb56b64adb9f
Kavin  B <131843294+kavinece2003@users.noreply.github.com>
2025-11-22 16:11:32 +0530

    Delete left_indicator.d

--- left_indicator.d
+++
@@ -1,6 +0,0 @@
-left_indicator.o: LEFT_INDICATOR.c
-left_indicator.o: C:\KeilARM\ARM\INC\Philips\lpc21xx.h
-left_indicator.o: CAN.h
-left_indicator.o: LCD_4.h
-left_indicator.o: C:\KeilARM\ARM\RV31\INC\stdio.h
-left_indicator.o: UART0.c
